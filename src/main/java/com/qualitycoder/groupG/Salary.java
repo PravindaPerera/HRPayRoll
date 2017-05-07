ec750a67d7ef128ecf536c77b2be581156563f2f
pravindap <pravinda.perera@trycake.com>
2017-05-07 12:30:30 +0530

    Pay sheet complete

--- src/main/java/com/qualitycoder/groupG/Salary.java
+++ src/main/java/com/qualitycoder/groupG/Salary.java
@@ -4,7 +4,16 @@ package com.qualitycoder.groupG;
  * Created by pravindap on 5/7/17.
  */
 public class Salary {
-    public PaySheet calculateSalary(){
-        return new PaySheet();
+    private Double salary;
+    private Double additions;
+
+    public Double calculateNetSalary(Double grossSalary, Double salaryAddition){
+        this.salary = grossSalary;
+        return this.CalculateAdditions(salaryAddition);
+    }
+
+    public Double CalculateAdditions(Double salaryAddition){
+        salary += salaryAddition;
+        return salary;
     }
 }
